6a00b25c647de6a89d63d6cca53a930f1fd58090
Harshad Hegde <hegdehb@gmail.com>
2024-02-09 04:16:47 -0600

    First commit

---
+++ src/optimgpt/cli.py
@@ -0,0 +1,45 @@
+"""Command line interface for OptimGPT."""
+
+import logging
+
+import click
+
+from optimgpt import __version__
+from optimgpt.main import demo
+
+__all__ = [
+    "main",
+]
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+@click.option("-v", "--verbose", count=True)
+@click.option("-q", "--quiet")
+@click.version_option(__version__)
+def main(verbose: int, quiet: bool):
+    """
+    CLI for OptimGPT.
+
+    :param verbose: Verbosity while running.
+    :param quiet: Boolean to be quiet or verbose.
+    """
+    if verbose >= 2:
+        logger.setLevel(level=logging.DEBUG)
+    elif verbose == 1:
+        logger.setLevel(level=logging.INFO)
+    else:
+        logger.setLevel(level=logging.WARNING)
+    if quiet:
+        logger.setLevel(level=logging.ERROR)
+
+
+@main.command()
+def run():
+    """Run the OptimGPT's demo command."""
+    demo()
+
+
+if __name__ == "__main__":
+    main()
