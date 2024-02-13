fd847c68bb2b5d3a82b0eb3495a4bce111de5961
Harshad Hegde <hegdehb@gmail.com>
2024-02-13 07:49:14 -0600

    Add `anthropic` support

--- src/codergpt/constants.py
+++ src/codergpt/constants.py
@@ -13,7 +13,7 @@ INSPECTION_HEADERS = ["File", "Language"]
 GPT_3_5_TURBO = "gpt-3.5-turbo"
 GPT_4 = "gpt-4"
 GPT_4_TURBO = "gpt-4-turbo-preview"
-CLAUDE = "claude"
+CLAUDE = "claude-2"
 GEMINI = "gemini-pro"
 
 ALL_MODELS = [
